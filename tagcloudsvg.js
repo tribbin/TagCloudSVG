ed0885f7734c95d5e696e076ffe139b91de00d85
admin <admin@admins-MacBook-Pro-9.local>
2017-02-21 14:11:44 +0100

    Added getJSON() function to make life easier without using jQuery.

--- tagcloudsvg.js
+++ tagcloudsvg.js
@@ -164,7 +164,7 @@ function sortTags() {
 
 	// Re-append the tags' SVG text-elements in order. Appended DOM-nodes are not copied, but moved.
 	for (var i=0; i<tags.length; i++) {
-		svg.appendChild(tags[i]);
+		svg.appendChildBefore(tags[i]);
 	}
 
 }
@@ -281,6 +281,26 @@ function nextFrame() {
 	}
 
 }
+
+/* ===================================================================
+   Code under this line is supporting code to make your life easier.
+   =================================================================== */
+
+var getJSON = function(url, callback) {
+	var xhr = new XMLHttpRequest();
+	xhr.open('GET', url, true);
+	xhr.responseType = 'json';
+	xhr.onload = function() {
+		var status = xhr.status;
+		if (status == 200) {
+			callback(null, xhr.response);
+		} else {
+			callback(status);
+		}
+	};
+	xhr.send();
+};
+
 
 /* ===================================================================
    Code under this line is a piece of altered code from TagCanvas 1.12
