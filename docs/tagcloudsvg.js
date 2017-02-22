95b4f2f53f0b2c1aac2a7c6be479a9bd5afb1b1b
Robin van Westrenen-Broekmans <robin@w530.tribbin.nl>
2017-02-22 23:42:05 +0100

    Added readystate-check in getJSON()

--- docs/tagcloudsvg.js
+++ docs/tagcloudsvg.js
@@ -311,7 +311,7 @@ function getJSON(url, callback = null) {
 	xhr.open('GET', url, true);
 	xhr.responseType = 'json';
 	xhr.onload = function() {
-		if (xhr.status == 200) {
+		if (xhr.readyState = 4 && xhr.status == 200) {
 			if(callback) {
 				callback(null, xhr.response);
 			} else {
